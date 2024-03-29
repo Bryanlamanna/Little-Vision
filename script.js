ba84eda486e6631cb57eec6dc25eddc3578eb7a9
Bryan Lamanna <lamannabryan@gmail.com>
2024-03-29 13:29:37 -0300

    update manifest.json

--- script.js
+++ script.js
@@ -8,15 +8,95 @@ const loadIcon = document.querySelector("#spinner");
 const copyIcon = document.querySelector(".copyBtn");
 const shareBtn = document.querySelector(".shareBtn");
 const botoesResponse = document.querySelector(".responseBtn");
-const persona = document.getElementById("persona");
+const persona = document.getElementById("personaSelect");
 const objetivo = document.getElementById("objetivo");
 const estadoEmocional = document.getElementById("estadoEmocional");
 const areaInteresse = document.getElementById("areaInteresse");
 const copySpan = document.querySelector("#copySpan");
+const interesses = document.querySelectorAll(".interesse");
+const emocoes = document.querySelectorAll(".emocao");
+const perconas = document.querySelectorAll(".persona");
+const interesseDiv = document.querySelector(".interessesDiv");
+const emocoesDiv = document.querySelector(".emocoesDiv");
+const personasDiv = document.querySelector(".personasDiv");
+const chevron = document.querySelectorAll("li i");
 var name = localStorage.getItem('name');
+var dropDown = [false, false, false];
 loadConfig();
 getNameOnCache();
 
+
+interesses.forEach((interesse) => {
+    interesse.addEventListener("click", (e) => {
+        document.querySelector(".selectedInteresseText").innerHTML = interesse.innerHTML;
+    })
+})
+
+emocoes.forEach((emocao) => {
+    emocao.addEventListener("click", (e) => {
+        document.querySelector(".selectedEmotionText").innerHTML = emocao.innerHTML;
+    })
+})
+
+perconas.forEach((persona) => {
+    persona.addEventListener("click", (e) => {
+        document.querySelector(".selectedPersonaText").innerHTML = persona.innerHTML;
+    })
+})
+
+
+persona.addEventListener("click", (e) => {
+
+    if (dropDown[2]=== false) {
+        personasDiv.style.height = 'fit-content';
+        interesseDiv.style.height = '0%';
+        emocoesDiv.style.height = '0%';
+        chevron[2].style.transform = 'rotate(180deg)';
+        dropDown[1] = false;
+        dropDown[0] = false;
+        dropDown[2] = true;
+    } else {
+        chevron[2].style.transform = 'rotate(0deg)';
+        personasDiv.style.height = '0%';
+        dropDown[2] = false;    
+    }
+
+})
+
+estadoEmocional.addEventListener("click", (e) => {
+    
+    if (dropDown[1]=== false) {
+        emocoesDiv.style.height = 'fit-content';
+        interesseDiv.style.height = '0%';
+        personasDiv.style.height = '0%';
+        chevron[1].style.transform = 'rotate(180deg)';
+        dropDown[0] = false;
+        dropDown[2] = false;
+        dropDown[1] = true;
+    } else {
+        chevron[1].style.transform = 'rotate(0deg)';
+        emocoesDiv.style.height = '0%';
+        dropDown[1] = false;
+    }
+})
+
+areaInteresse.addEventListener("click", (e) => {
+    
+    if (dropDown[0]=== false) {
+        interesseDiv.style.height = 'fit-content';
+        personasDiv.style.height = '0%';
+        emocoesDiv.style.height = '0%';
+        chevron[0].style.transform = 'rotate(180deg)';
+        dropDown[1] = false;
+        dropDown[2] = false;
+        dropDown[0] = true;
+    } else {
+        chevron[0].style.transform = 'rotate(0deg)';
+        interesseDiv.style.height = '0%';
+        dropDown[0] = false;
+    }
+
+})
 
 function getNameOnCache() {
     if (name === '' || name === null || name === undefined) {
@@ -107,10 +187,11 @@ function montarPrompt() {
     if (persona.value === "Groot") {
         prompt = 'o que o Groot diria?';
     } else {
-        prompt = `Escreva uma frase motivacional sobre ${areaInteresse.value}, fale como se voce fosse o ${persona.value}. 
-                    leve em consideração estas informações: 
-                    Meu nome é ${name}.
-                    Meu atual objetivo é ${objetivo.value} e hoje me sinto ${estadoEmocional.value}.`;
+        prompt = `Meu nome é ${name}.
+                Escreva uma frase motivacional sobre ${document.querySelector(".selectedInteresseText").innerHTML}, fale como se voce fosse o ${document.querySelector(".selectedPersonaText").innerHTML}. 
+                leve em consideração estas informações: 
+                
+                Meu atual objetivo é ${objetivo.value} e hoje me sinto ${document.querySelector(".selectedEmotionText").innerHTML}.`;
     }
         console.log(prompt);
      return prompt;         
